4651f501d9242658b90983618972ccf4dcc78a42
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-10-29 19:15:51 +0530

    Fixing bug - BE

--- BE/index.js
+++ BE/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 require('dotenv').config()
 
 // middleware
